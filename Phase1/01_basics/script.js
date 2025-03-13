28dea03ae67c09edeb6667e302bfc11beba0eb92
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2025-03-13 11:55:55 +0000

    Learning about hoisting and TDZ

--- Phase1/01_basics/script.js
+++ Phase1/01_basics/script.js
@@ -4,7 +4,31 @@ const AlertBtn = document.getElementById('alert-btn');
 
 const Head1 = document.getElementById('head-1');
 
+// let userName=prompt("Enter Your Name BOSS!!");
+
+
 AlertBtn.addEventListener('click',()=>{
-   Head1.innerText=`This page belongs to ${name1}`;
+   Head1.innerText=`This page belongs to ${userName}`;
 // console.log("hello");
 })
+
+const fruits = ["apple","orange","banana","kiwi"];
+
+const FruitHeader = document.getElementById("Fruits-heading");
+
+fruits.map((item,index)=>{
+   FruitHeader.innerText=item;
+});
+
+console.log(name11);
+
+var name11 = "vivek";
+// let name22 = "sunny";
+// const name33 = "money";
+
+
+
+// var => redeclare also possible, updation also possible. 
+// let => redeclaration not possible, but updation possible.
+// const => neither, use when you want the data to remain unchanged
+
