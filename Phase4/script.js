10a4fbf9e64e88a72e022c175e97b4fd1aa69154
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2025-04-02 06:50:23 +0000

    Learning event loop and threading in js

--- Phase4/script.js
+++ Phase4/script.js
@@ -38,20 +38,54 @@ console.log("task3");
 
 // CALLBACKS
 
-// callbacks are functions passed as argument in another function
+setTimeout(function(){
+    console.log("I am an example of Callback function")
+},4000)
 
-// Callbacks are like "Call me back when it's done!" functions.
+// Toh callback kya hota hai se zyda confuse na hoke simple rakhte hai :- upar setTimeout mein jo function as an argument pass
+// kara hai, wohi ek callback function hai.
 
-function Hello(name,callback){
-    console.log(`hello! mr. ${name}`);
-    setTimeout(function(){
-        callback();
-    },2000)
-    
-}
+// callback ke naam se hi pata chal rha hai ek aisa function jisse after certain time or happening of a certain task call back kar rahe hai
 
-function Greet(){
-    console.log("I am good")
-}
+// In the above example, callback 4 seconds baad call ho rha hai, but it is not necessary ki har callback ki calling time dependent ho.
 
-Hello("vivek",Greet)
+// yaani async code mein callback humesha koi result aane pe chalta hai.
+
+// IMP POINT :- JS IS NOT ASYNCHRONOUS 
+
+// Now listen to important story :-
+
+// javascript ka code 2 parts mein divide ho sakta hai :- sync code and async code 
+
+// now behind the scenes kya kar rahi hai javascript, there are 2 stacks :- Main stack and Side Stack,
+
+// toh it is important to understand that jo bhi js ka sync code hoga it will always go into main stack, always. 
+
+// and then jo jo main stack mein hoga, woh ek ek karke execute hota rahega and usse stack se bahar karte rahenge. 
+
+// and only and only when yeh main stack empty hojayega js will go to side stack and check if there is something in side stack.
+
+// and side stack mein kya hoga, async code, saara async code side stack mein hoga, from there when its processing will complete  
+
+// usse main stack mein daala jayega. 
+
+// And this whole process of main stack and side stack conversing with each other where main stack constantly keeps checking for aysnc code
+
+// in side stack , is known as EVENT LOOP. Very very important concept.
+
+
+// Hence, js is not async...why??... Because js is single threaded. Single Thread ka matlab sirf ek hi computation karne ki power hai ek time pe.
+
+// A thread is like a worker that executes code.
+// Think of a thread as a single path of execution inside a program.
+
+// In simple terms, when you run a program, a thread is responsible for executing the instructions, one by one, from top to bottom.
+
+
+// JS is SINGLE-THREADED, meaning:
+
+//     One main thread (Call Stack) executes code sequentially.
+
+//     If one task takes too long, everything else gets blocked! 
+
+//     But JS avoids blocking by using ASYNC operations (Web APIs, Event Loop, Callbacks, Promises, etc.) 
