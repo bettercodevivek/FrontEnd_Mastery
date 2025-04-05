d2f721e7aea8af8ded2dc095af46ad83b2e2e0ba
Vivek Singh <149397884+bettercodevivek@users.noreply.github.com>
2025-04-05 19:31:19 +0000

    Learnt POST in fetch

--- Phase5/script.js
+++ Phase5/script.js
@@ -45,3 +45,84 @@ async function GetJoke(){
 }
 
 GetJoke()
+
+// So far we were only getting data from an API (using GET). But what if we want to send some data — like submitting a form, creating a new user, or posting a comment?
+
+// We use a POST request 
+
+// BASIC SYNTAX FOR A POST REQUEST :- 
+
+// fetch("https://example.com/api", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({ name: "Bro", age: 21 })
+//   });
+
+
+async function SubmitData(){
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/comments",
+            {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                body:JSON.stringify(
+                    {
+                        name:"josh",
+                        age:20,
+                        id:112,
+                    }
+                )
+            }
+        )
+
+        const result = await response.json()
+        console.log("Result Posted:",result)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+  
+
+SubmitData();
+
+// Explanation of above code :- 
+
+// method	               Tells the API we are sending data (POST)
+// headers	               Says what kind of data we're sending
+// body	                   The actual data, converted to JSON string
+// response.json()	       Converts the response into JS object
+
+// challenge 2
+
+// sending a POST request to an endpoint
+
+async function SubmitData2(){
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                title: "Learning Fetch is lit 🔥",
+                body: "With my G's help, I'm mastering JavaScript!",
+                userId: 99
+            })
+        })
+    
+        const result = await response.json()
+        console.log("Data Sent Success:-",result)
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+SubmitData2();
+
+// Let us now Study about Events in JS, basically all those events that can happen on a page in browser.
