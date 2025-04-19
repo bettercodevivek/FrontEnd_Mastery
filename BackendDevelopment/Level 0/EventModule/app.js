4bf0cc5b293c152eab76d4b59bdf59a836dbd665
bettercodevivek <viveksingh0520@gmail.com>
2025-04-19 13:33:28 +0530

    Learning more about Events Module

--- BackendDevelopment/Level 0/EventModule/app.js
+++ BackendDevelopment/Level 0/EventModule/app.js
@@ -28,6 +28,11 @@ const EventEmitter = require('events')
 
 const emitter1 = new EventEmitter();
 
+emitter1.on("Hello",()=>{
+   console.log("Event has been emitted and listened Boss !!")
+})
+
+
 // SIDE NOTE:- so suddenly mjhe new keyword ki significance ke baare mein jaane ka importance laga so here it is :- 
 
 // new ka use sirf tab hota hai jab tu kisi constructor function ya class se object create kar raha ho. 
@@ -70,15 +75,88 @@ const emitter1 = new EventEmitter();
 
  hello("Travis")
 
- class Cars{
-    constructor(carname){
-        this.carname=carname;
-    }
- }
+//  class Cars{
+//     constructor(carname){
+//         this.carname=carname;
+//     }
+//  }
 
- const car1 = Cars("toyota")
+//  const car1 = Cars("toyota")
 
- console.log(car1.carname) // Class constructor Cars cannot be invoked without 'new'
+//  console.log(car1.carname) // Class constructor Cars cannot be invoked without 'new'
 
  // so it is very important to understand when you are dealing with constructor functions, objects and classes, new keyword is a must
  // especially with classes, we can clearly see that without new we get the above error.
+
+ emitter1.emit("Hello")
+
+ // So , lets learn everything that we can about this events module
+
+ // 1. Basic Setup 
+
+//  const EventEmitter = require('events');
+//  const event = new EventEmitter();
+
+// 2. Registering an event listener
+
+//  event.on("eventname",()=>{
+//     console.log('event triggered !')
+//  })
+
+// on() is our standard listener and can run multiple times
+
+// 3. Triggering the event
+
+// event.emit("eventname")
+
+// Triggers all listeners attached to eventName
+
+// Sync execution, in order of registration
+
+// 4. Passing data to listeners
+
+// event.on('data',(msg,code)=>{
+//   console.log(`Message:${msg} and Code:${code}`)
+// });
+
+// event.emit('data','hello',200)
+
+// 5. one time listener ( this is perfect when you want something to happen only once like app started or db connected)
+
+// event.once('launch',()=>{
+//  console.log("this will run only once !")})
+
+// 6. removing listeners
+
+// event.off() or event.removeListener() can be used when we no longer wish to listen to an event.
+
+// 7. getting listeners info
+
+// console.log(event.eventNames()); List of all event names
+// console.log(event.listenerCount('eventName')); How many listeners on an event
+
+// 8. Listener leak warning
+
+// toh basically hota kya hai, agar hum kisi event emission pe, 10 se zyda listeners (on()) lagade, toh node js will give us a warning
+// it may look like this :- (node:12873) MaxListenersExceededWarning: Possible EventEmitter memory leak detected.
+
+// To fix this :-
+
+// event.setMaxListeners(20); // you can set the max limit for listeners as per you convinience
+
+// So, LETS DO A CHALLENGE TO CEMENT THIS CONCEPT OF EVENT MODULES :-
+
+// Simulate a file downloading system using EventEmitter where different parts of the system react to events like 
+// start, progress, error, and complete.
+
+const eventEmitter1 = require('events');
+
+class DownloadManager extends eventEmitter1{
+      startDownload(){
+         event
+      }
+}
+
+
+
+
