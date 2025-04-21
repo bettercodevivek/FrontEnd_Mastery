d0e13fa4472b5b23981b5aa43320979647a0eff1
bettercodevivek <viveksingh0520@gmail.com>
2025-04-22 01:26:21 +0530

    Learnt about multiple routes in http module

--- BackendDevelopment/Level 0/HttpModule/server.js
+++ BackendDevelopment/Level 0/HttpModule/server.js
@@ -15,16 +15,16 @@ const http = require('http');
 
 // The actual code goes like this :-
 
-const server = http.createServer((req,res)=>{
-    console.log(`request recieved :- Method - ${req.method} and URL - ${req.url}`)
+// const server = http.createServer((req,res)=>{
+//     console.log(`request recieved :- Method - ${req.method} and URL - ${req.url}`)
 
-    res.writeHead(200,{'content-type':'text/plain'});
-    res.end("Hello from Vivek's first server !")
-})
+//     res.writeHead(200,{'content-type':'text/plain'});
+//     res.end("Hello from Vivek's first server !")
+// })
 
-server.listen(3000,()=>{
-    console.log("Server Started at Port: 3000");
-});
+// server.listen(3000,()=>{
+//     console.log("Server Started at Port: 3000");
+// });
 
 // ab upar yeh jo callback hai createServer() mein, isme yeh (req,res)=>{} , jo hai it signifies the following:-
 
@@ -36,3 +36,46 @@ server.listen(3000,()=>{
 // res is actually an object of class = http.ServerResponse , used by server to send response to client side
 
 // createServer() is not an async function by default but uske andar jo callback hai, we can make that an async if need arises.
+
+// let us understand how we can create multiple routes 
+
+// When you create a server with Node's native http module, you handle routing manually. This means:
+
+// You check the req.url and req.method.
+
+// Based on the URL (and sometimes method), you respond differently.
+
+// example code for manual multiple routes :- 
+
+
+const server = http.createServer((req,res)=>{
+    if(req.url === '/' && req.method === 'GET'){
+      res.writeHead(200,{"content-type":"text/plain"})
+      res.end("This is the homepage of vivek's server !")
+    }
+    else if(req.url === '/about' && req.method === 'GET'){
+        res.writeHead(200,{"content-type":"text/plain"})
+        res.end("This is the about page of vivek's server !")
+    }
+    else if(req.url === '/contact' && req.method === 'GET'){
+        res.writeHead(200,{"content-type":"text/plain"})
+        res.end("This is the contact page of vivek's server !")
+    }
+})
+
+server.listen(3000,()=>{
+    console.log("Server started on port : 3000")
+})
+
+// here listen function takes the port number and a callback function. 
+
+// it works in this way = "Start listening for incoming requests on this port!"
+
+// What it does:
+
+// Binds the server to the specified port.
+
+// Keeps the process running so it can handle future incoming requests.
+
+// Executes the callback when the server is ready to accept requests (not when a request is received).
+
