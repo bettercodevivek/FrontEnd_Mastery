77e05e81c3a4a56fd8f3433b031e803987a92b9f
bettercodevivek <viveksingh0520@gmail.com>
2025-04-24 12:00:58 +0530

    Implementing a basic Notes API

---
+++ BackendDevelopment/Level 0/HttpModule/notesapi.js
@@ -0,0 +1,9 @@
+// We’ll build a local HTTP server that supports these routes and methods:
+
+// Method        | Route            | Purpose
+// GET           | /notes           | Get all notes
+// GET           | /notes/:id       | Get a single note by ID
+// POST          | /notes           | Add a new note
+// DELETE        | /notes/:id       | Delete a note by ID
+
+
