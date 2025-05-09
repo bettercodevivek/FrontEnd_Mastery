eb427e76cc2b3df7d233f81a4097b8264a58d202
bettercodevivek <viveksingh0520@gmail.com>
2025-05-09 19:32:08 +0530

    Built-In and Third party middlewares implemented !

--- BackendDevelopment/Level1/Express_Basics/MiddlewareDeep.js
+++ BackendDevelopment/Level1/Express_Basics/MiddlewareDeep.js
@@ -30,6 +30,7 @@ const app = express();
 
 const PORT = 8080;
 
+
 app.use((req,res,next)=>{
   console.log(` Request has been made at URL: ${req.url} and METHOD used: ${req.method} at TIME: ${new Date().toLocaleString()} using IP: ${req.ip}`)
   next();
@@ -75,3 +76,62 @@ app.listen(PORT,()=>{
 //  next() nahi chala → aage ke routes kabhi nahi milenge, because yeh simple hai bhaut, response agar middleware hi send kar raha hai
 // iska simple meaning yehi hai ki request couldnt pass through middleware perfectly !
 
+// 2. ROUTE SPECIFIC MIDDLEWARES
+
+//  As the name suggests, yeh woh middlewares hai jo hum kisi particular route pe lagayenge bas !
+
+// example:- 
+
+const myLogger = (req,res,next) =>{
+    console.log(`This Request is being logged by MyLogger --- URL:${req.url} and METHOD: ${req.method}`);
+    next();
+}
+
+const myLogger2 = (req,res,next) => {
+    console.log(`This is MyLogger2 and this proves that multiple middlewares can be written for the same route`);
+    next();
+}
+
+app.get('/about',myLogger,myLogger2,(req,res)=>{
+    res.status(200).send("Welcome to the about page !")
+})
+
+// Toh 2 imp cheezein :- specific route ke liye uska middleware path aur handler function ke beech mein lagaya jaa sakta hai
+// and path aur handler function ke beech jitne chahe utne middlewares lagaye jaa sakte hai
+
+// similarly, ek hi middleware multiple routes pe lagaya jaa sakta hai
+
+// Also, agar multiple middlewares lagane hai ek route pe, array of those middlewares can also be given
+
+// const mw1 = (req, res, next) => { console.log('mw1'); next(); };
+// const mw2 = (req, res, next) => { console.log('mw2'); next(); };
+
+// app.get('/multi', [mw1, mw2], (req, res) => {
+//     res.send('Multiple middleware!');
+// });
+
+// Now, built-in and 3rd party middlewares in express js
+
+// Express kuch middleware already provide karta hai → hume install ya define nahi karna padta → bas use karna padta hai.
+
+// Examples:
+
+// Built-in Middleware	                 Kaam kya karta hai
+
+// express.json()	                     JSON body ko parse karta hai → req.body me set karta hai
+// express.urlencoded()	                 URL-encoded data parse karta hai (form data ke liye)
+// express.static()	                     Static files serve karta hai (images, CSS, JS, etc.)
+
+// app.use(express.static('public'));
+
+// Ab tum public folder ke andar jo bhi file rakho → browser me /filename pe access kar sakte ho.
+
+// 3rd party => Ye middleware npm se install kiye jaate hain → additional functionality ke liye.
+
+// example of morgan :- 
+
+// const morgan = require('morgan');
+
+// app.use(morgan('dev'));
+
+// bas yeh jo upar likha hai, yeh saari requests likhne se pehle likhdena tabhi it will work.
