d688c921291e05afd016e9cc2c89d8ae65964fcc
bettercodevivek <viveksingh0520@gmail.com>
2025-05-17 22:45:15 +0530

    Getting started with NoteModel and UserModel for NotesAPI

---
+++ BackendDevelopment/Level1/Revision-Mini-Projects/Notes-API-Mini-Project/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+const jwt = require('jsonwebtoken');
+
+const bcrypt = require('bcryptjs');
+
+const app = express();
+
+const PORT = 8080;
+
+// sabse pehle ek application level built-in middleware implement kardete hai
+
+app.use(express.json());
+
+// so we need to build a notes api : meaning CRUD operations of notes, login and signup with jwt.
+
+// Establish a connection with DB first
+
+mongoose.connect('mongodb://localhost:27017/')
+.then(()=>{
+    console.log("Established Connection with DB Successfully !")
+})
+.catch((err)=>{
+ console.log("An Error Occurred While connecting with DB : ",err)
+});
+
+// We will write the home (/) GET route first
+
+app.get('/',(req,res)=>{
+    res.status(200).json({message:"WELCOME TO THE HOME PAGE OF NOTES API !"})
+});
+
+// we will write (/signup) POST route now
+
+app.post('/signup',async(req,res)=>{
+
+    const {username,email,password} = req.body;
+    console.log(req.body);
+
+    try{
+      if(!username || !email || !password){
+        return res.status(400).json({error:"All Credentials need to be filled !"})
+      }
+
+      const note = 
+
+    }
+    catch(err){
+
+    }
+});
