613f0aeaf73d6093b7a7765fe0f9c8d03e0541c9
bettercodevivek <viveksingh0520@gmail.com>
2025-05-18 13:25:30 +0530

    Wrote Auth Middleware and Notes POST route

---
+++ BackendDevelopment/Level1/Revision-Mini-Projects/Notes-API-Mini-Project/AuthMiddleware.js
@@ -0,0 +1,32 @@
+// Now, we will create this middleware that will validate the jwt created at login , for every protected route
+
+const jwt = require('jsonwebtoken');
+
+const SECRET_KEY = 'abcdef1234';
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const AuthMiddleware = (req,res,next) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        return res.status(401).json({error:"Header not found !"})
+    }
+       
+    const token = authHeader.split(" ")[1];
+
+    try{
+        const decode = jwt.verify(token,SECRET_KEY);
+
+        req.user = decode;
+
+        next();
+    }
+    catch(err){
+        res.status(403).json({error:"Invalid or expired token !"})
+    }
+}
--- BackendDevelopment/Level1/Revision-Mini-Projects/Notes-API-Mini-Project/server.js
+++ BackendDevelopment/Level1/Revision-Mini-Projects/Notes-API-Mini-Project/server.js
@@ -6,7 +6,14 @@ const jwt = require('jsonwebtoken');
 
 const bcrypt = require('bcryptjs');
 
+const User = require('./UserModel');
+
 const app = express();
+
+const AuthMiddleware = require('./AuthMiddleware');
+const Note = require('./NoteModel');
+
+const SECRET_KEY = "abcdef1234"
 
 const PORT = 8080;
 
@@ -35,9 +42,110 @@ app.get('/',(req,res)=>{
 // we will write (/signup) POST route now
 
 app.post('/signup',async(req,res)=>{
-  
+  try{
+  const {username,email,password} = req.body;
+   console.log(req.body);
+      if(!username || !email || !password){
+    return res.status(409).json({error:"Enter All Credentials"});
+   }
+
+   const userExists = await User.findOne({email});
+
+   if(userExists){
+    return res.status(404).json({error:"This user already exists, kindly login !"})
+   }
+    
+   const newUser = new User({username,email,password});
+
+   await newUser.save()
+
+   res.status(201).json({
+    message:" Signup Successful !",
+    user:{
+      username:newUser.username,
+      email:newUser.email
+    }
+   });
+    }
+    catch(err){
+      res.status(500).json({error:"Internal Server Error !"})
+    }
 })
 
+
+// we will write login (POST) route now
+
+app.post('/login',async(req,res)=>{
+
+ const {email,password} = req.body;
+ console.log(req.body);
+
+ try{
+  
+   if(!email || !password){
+    return res.status(400).json({error:"Fill Both the credentials !"})
+   }
+
+   const user = await User.findOne({email});
+
+   if(!user){
+    return res.status(404).json({error:"This user doesn't exist, Please SignUp First !"})
+   }
+
+   const isMatch = await bcrypt.compare(password,user.password);
+
+   if(!isMatch){
+    return res.status(404).json({error:"Wrong Password !"})
+   }
+
+   const payload = {
+    userId:user._id,
+    username:user.username,
+    email:user.email
+   }
+
+   const token = jwt.sign(payload,SECRET_KEY,{expiresIn:"1h"})
+    
+   res.status(200).json({
+    message:"Login Successful !",
+    user:{
+      token:token,
+      username:user.username,
+      email:user.email
+    }
+   });
+ }
+ catch(err){
+  res.status(500).json({error:"Internal Server Error !"});
+ }
+})
+
+// now we will start with notes routes and we will apply authmiddleware to each route as each route is protected in this scenario
+
+app.post('/notes',AuthMiddleware,async(req,res)=>{
+    const {title,content} = req.body;
+    try{
+      
+      if(!title || !content){
+        return res.status(401).json({error:"Both Fields need to be filled !"})
+      }
+
+      const newNote = new Note({title,content});
+
+      await newNote.save();
+       
+      res.status(201).json({
+        message:"New Note Created Successfully !",
+        note:{
+          title:newNote.title,
+          content:newNote.content
+        }
+      });
+    }
+    catch(err){
+        res.status(500).json({error:"Internal Server Error !"})
+    }
+})
 
 app.listen(PORT,()=>{
   console.log(`Server successfully started at PORT : ${PORT}`)
